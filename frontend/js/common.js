2f58f0be26e7d67ad1f345e826d6f9f4e595cc95
lokesh <lokeshmendudala9@gmail.com>
2026-02-24 22:19:07 +0530

    modified.

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -1,4 +1,4 @@
-const API_BASE =  'https://career-guidance-and-resume-evalu.railway.internal';
+const API_BASE =  'https://career-guidance-and-resume-evaluator-production-2d2f.up.railway.app';
 
 function requireAuth() {
     if (!localStorage.getItem('token')) window.location.href = 'login.html';
